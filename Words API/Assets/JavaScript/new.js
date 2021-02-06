e180aa5689fc5fcbc64cc55948e6a7fa073432c2
rahul2-byte <rahulchand4299@gmail.com>
2021-02-06 22:44:36 +0530

    new
    Some addjestment in JS.

--- Words API/Assets/JavaScript/new.js
+++ Words API/Assets/JavaScript/new.js
@@ -1,57 +1,82 @@
-// window.addEventListener("load", () => {
-//   fetch("https://type.fit/api/quotes")
-//     .then((response) => {
-//       return response.json();
-//     })
-//     .then((data) => {
-//       const number = Math.floor(Math.random() * data.length);
-//       console.log(number);
-//       const center_txt = document.querySelector(".main-text");
-//       center_txt.innerHTML = `&#8220` + data[number].text + ` &#8221`;
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//     });
-// });
+window.addEventListener("load", () => {
+  fetch("https://type.fit/api/quotes")
+    .then((response) => {
+      console.log(response);
+      return response.json();
+    })
+    .then((data) => {
+      const number = Math.floor(Math.random() * data.length);
+      console.log(number);
+      const center_txt = document.querySelector(".main-text");
+      center_txt.innerHTML = `&#8220` + data[number].text + ` &#8221`;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 // const search_input_value = document.querySelector(".search-value").value;
 // console.log("h2");
 // console.log(search_input_value);
 // console.log("hlw");
-// Event triggers when select-box is changed.
-{
-  const select_box = document.querySelector(".select-box");
-  select_box.addEventListener("change", (e) => {
-    console.log("Item changed");
-  });
-}
+
 // Event trigers when we search the word.
 {
   const form = document.querySelector("form");
   const inputVar = document.querySelector("input[type = text]");
-  console.log(inputVar);
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     console.log(inputVar.value.trim());
+    const Meaningdiv = document.querySelector(".meaning-container");
+    console.log(Meaningdiv);
+    // Checking if Meaning div is present in DOM tree
     if (!inputVar.value.trim()) {
-      const search_info = document.getElementById("search-info");
+      const search_info = document.getElementById("search-info"); // Get the info for search-bar.
       search_info.className = "search-info-show";
-      console.log("Search value is undefined");
+      const meaning_div = document.querySelector(".meaning-container");
+      console.log(meaning_div);
+      meaning_div.className = "Meaning-display-none";
+      /*
+       * To remove the Meaning div from page.
+       */
     } else {
-      console.log("This is else statement.");
-      const search_info = document.getElementById("search-info");
-      search_info.className = "search-info-hide";
+      if (typeof Meaningdiv !== undefined && Meaningdiv !== null) {
+        console.log("Here ELement is present.");
+      } else {
+        /*
+         *To Create the Meaning div.
+         */
+        const search_info = document.getElementById("search-info");
+        search_info.className = "search-info-hide";
+        meaningDiv(); // Calling function to create  div for Meanings.
+      }
     }
   });
 }
-// Event that will create new div for Meanings.
-{
-  const body = document.querySelector("body");
-  const newDiv = document.querySelector(".main");
+
+/*
+ *---------------------------------------------------------
+ * /// Function to create Meaning Div.///
+ * --------------------------------------------------------
+ */
+
+const meaningDiv = function () {
+  const body = document.querySelector(".main");
+  console.log(body.children);
   const div = document.createElement("div");
   div.className = "meaning-container";
-  div.appendChild(document.createTextNode("hello"));
-  body.appendChild(div);
-}
+  div.innerHTML = `<div class="meaning-container">
+  <div class="main-aria">
+      <p class="definitions mxy-1 pxy-1 bold-words" id="Searched-word" > Word : <span class="span-definitions small-words" id="word-definition"> Defi </span></p>
+      <p class="partsofspeech mxy-1 pxy-1 bold-words"> Part of Speech : <span class="span-partofspeech small-words" id="word-partofspeech"> Noun </span></p>
+      <p class="synonyms mxy-1 pxy-1 bold-words"> Synonyms : <span class="span-synonyms small-words" id="word-synonyms"> Synonyms </span></p>
+      <p class="examples mxy-1 pxy-1 bold-words"> Example : <span class="span-example small-words" id="word-example"> Word examples </span></p>
+  </div>
+</div>`;
+  const TxtNode = document.createTextNode(div);
+  div.appendChild(TxtNode);
+  body.insertBefore(div, body.children[5]);
+};
+
 // const search_value = document.getElementsByClassName("submit-button").value;
 // const option_element = document.getElementsByClassName("options");
 // option_value = option_element.options[option_element.selectedIndex].value;
